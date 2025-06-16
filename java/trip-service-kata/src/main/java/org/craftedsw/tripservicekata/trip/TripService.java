e31981f744d731b3acfa1f7dd6d8264632a2ba70
pranjal-caizin <pranjal@caizin.com>
2025-06-16 13:50:19 +0530

    Pranjal | Refactors the TripService class

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -10,23 +10,19 @@ import org.craftedsw.tripservicekata.user.UserSession;
 public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
-		List<Trip> tripList = new ArrayList<Trip>();
+		List<Trip> tripList = new ArrayList<>();
 		User loggedUser = UserSession.getInstance().getLoggedUser();
-		boolean isFriend = false;
-		if (loggedUser != null) {
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
-			if (isFriend) {
-				tripList = TripDAO.findTripsByUser(user);
-			}
-			return tripList;
-		} else {
+
+		if(loggedUser ==  null) {
 			throw new UserNotLoggedInException();
 		}
+
+		boolean isFriend = user.getFriends().contains(loggedUser);
+		if (isFriend) {
+			tripList = TripDAO.findTripsByUser(user);
+		}
+
+		return tripList;
 	}
-	
+
 }
