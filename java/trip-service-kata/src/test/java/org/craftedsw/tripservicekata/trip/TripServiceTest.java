00638a8aae1ef28c8501b11f996b3fe8ba212d8d
pranjal-caizin <pranjal@caizin.com>
2025-06-20 09:26:48 +0530

    Pranjal | - Add test for getting trips when logged-in user is a friend of target user - Upgrade pom.xml

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -4,6 +4,8 @@ import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSessionProvider;
 import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -23,5 +25,23 @@ public class TripServiceTest {
         assertThrows(UserNotLoggedInException.class, () -> {
             tripService.getTripsByUser(user);
         });
+    }
+
+    @Test
+    public void shouldReturnTripsWhenLoggedUserIsAFriend() {
+        User loggedUser = new User();
+        User otherUser = new User();
+        otherUser.addFriend(loggedUser);
+
+        Trip trip1 = new Trip();
+        Trip trip2 = new Trip();
+        List<Trip> expectedTrips = List.of(trip1, trip2);
+
+        when(userSessionProvider.getLoggedUser()).thenReturn(loggedUser);
+        when(tripProvider.findTripsByUser(otherUser)).thenReturn(expectedTrips);
+
+        List<Trip> actualTrips = tripService.getTripsByUser(otherUser);
+
+        assertEquals(expectedTrips, actualTrips);
     }
 }
