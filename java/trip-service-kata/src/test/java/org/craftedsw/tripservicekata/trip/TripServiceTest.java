ad51753ca3c098bad1be7ce5cdf639d51e6ab646
pranjal-caizin <pranjal@caizin.com>
2025-06-24 13:42:08 +0530

    Pranjal | Adds a test case which return trips when users are friends.

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,9 +1,12 @@
 package org.craftedsw.tripservicekata.trip;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.trip.model.Trip;
 import org.craftedsw.tripservicekata.trip.service.TripService;
 import org.craftedsw.tripservicekata.user.model.User;
 import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TripServiceTest {
@@ -16,11 +19,30 @@ public class TripServiceTest {
         assertThrows(UserNotLoggedInException.class, () -> testableTripService.getTripsByUser(user));
     }
 
+    @Test
+    void shouldReturnTripsWhenUsersAreFriends() {
+        User loggedInUser = new User();
+        User otherUser = new User();
+        TestableTripService testableTripService = new TestableTripService();
+
+        otherUser.addFriend(loggedInUser);
+        otherUser.addTrip(new Trip());
+
+        List<Trip> trips = testableTripService.findTripsByUser(otherUser);
+
+        assertEquals(1, trips.size());
+    }
+
     private static class TestableTripService extends TripService {
 
         @Override
         protected User getLoggedUser() {
             return null;
+        }
+
+        @Override
+        protected List<Trip> findTripsByUser(User user) {
+            return user.trips();
         }
     }
 }
